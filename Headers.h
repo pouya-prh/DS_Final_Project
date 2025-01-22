b57253219cbecd0974a6e68c26eede66addb4ca1
pouya-prh <pouya.bishe2@gmail.com>
2025-01-22 13:51:25 +0330

    definition of "Movies" class and its functions

--- Headers.h
+++ Headers.h
@@ -1,17 +1,18 @@
 #pragma once
 using namespace std;
+
 #include <iostream>
 #include <string>
 #include <conio.h>
 #include <windows.h> 
 #include <fstream>
 #include "json.hpp"
+#include <vector>
+
 //for Sing up`s password hash function
 #include <openssl/sha.h>
 #include <iomanip>
 #include <sstream>
 //--------------------------
-
-
 
 using json = nlohmann::json;
